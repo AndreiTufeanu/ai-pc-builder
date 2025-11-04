4d950d3dbd90a71cecda6b011478a02a626ac7fe
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2025-11-04 21:33:27 +0200

    chore: add base project and modify gitignore

---
+++ src/main/java/com/example/aipcbuilder/AipcBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.example.aipcbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AipcBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AipcBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/aipcbuilder/AipcBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aipcbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AipcBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

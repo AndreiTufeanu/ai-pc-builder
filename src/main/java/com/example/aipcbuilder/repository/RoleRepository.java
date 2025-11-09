6be7cafa2748dc8c279897cb993f729717fff7db
AndreiTufeanu <andreitufeanu13@gmail.com>
2025-11-09 17:20:14 +0200

    feat: add repositories for User and Role

---
+++ src/main/java/com/example/aipcbuilder/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.example.aipcbuilder.repository;
+
+import com.example.aipcbuilder.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(String name);
+}

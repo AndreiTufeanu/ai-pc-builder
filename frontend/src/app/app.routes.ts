570f1e2d56e8954f8b3e1c68dbe5925993b73ff8
AndreiTufeanu <andreitufeanu13@gmail.com>
2025-11-10 16:04:17 +0200

    feat: update app component related files

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from './features/welcome/welcome.component';
+import { LoginComponent } from './features/auth/login/login.component';
+import { SignupComponent } from './features/auth/signup/signup.component';
+import { AdminDashboardComponent } from './features/admin-dashboard/admin-dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'admin', component: AdminDashboardComponent },
+  { path: '**', redirectTo: '' }
+];

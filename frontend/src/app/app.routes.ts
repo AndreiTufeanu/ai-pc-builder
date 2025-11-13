9e00e28e66d95140f37a7d481d2d0aaeab347e84
AndreiTufeanu <andrei.tufeanu13@gmail.com>
2025-11-13 17:57:05 +0200

    feat: update routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { WelcomeComponent } from './features/welcome/welcome.component';
 import { LoginComponent } from './features/auth/login/login.component';
 import { SignupComponent } from './features/auth/signup/signup.component';
 import { AdminDashboardComponent } from './features/admin-dashboard/admin-dashboard.component';
+import { UserDashboardComponent } from './features/user-dashboard/user-dashboard.component';
 
 export const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent },
   { path: 'admin', component: AdminDashboardComponent },
+  { path: 'user-dashboard', component: UserDashboardComponent },
   { path: '**', redirectTo: '' }
 ];

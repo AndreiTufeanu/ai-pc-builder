f80c3055083dd8f4ec95d6a0c01c6374ab9988e4
AndreiTufeanu <andreitufeanu13@gmail.com>
2025-12-04 19:53:09 +0200

    feat: add validation to the fields of SignupRequest and LoginRequest dtos

--- backend/src/main/java/com/example/aipcbuilder/dto/LoginRequest.java
+++ backend/src/main/java/com/example/aipcbuilder/dto/LoginRequest.java
@@ -1,20 +1,21 @@
 package com.example.aipcbuilder.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
-@Setter
-@Getter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 3, message = "Password must be at least 3 characters")
     private String password;
-
-    // Constructors
-    public LoginRequest() {}
-
-    public LoginRequest(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
 }
